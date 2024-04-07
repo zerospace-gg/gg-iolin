68a5f04ac145de38a061699595a8253df54eb677
rubythulhu <95746090+rubythulhu@users.noreply.github.com>
2024-04-06 22:26:13 -0700

    ooh i build an index now

--- buildall.sh
+++ buildall.sh
@@ -2,9 +2,14 @@
 
 set -euo pipefail
 VERSION=$(<zsgg_data_version.txt)
-AT=$(date +%Y%m%d_%s)
+AT=$(date +%Y%m%d%H%M%S)
 
-for i in $(find zerospace -name '*.pkl'); do
-  printf "# \e[0;1;33m%s\e[0m\n" "$i"
-  pkl eval -m ./dist -p gg_version="$VERSION" -p gg_at="$AT" "$i"
-done
+time (
+  for i in $(find zerospace -name '*.pkl' | sort); do
+    printf "\n# \e[0;1;33m%s\e[0m\n" "$i"
+    pkl eval -m ./dist -p gg_version="$VERSION" -p gg_at="$AT" "$i"
+    printf "\n"
+  done
+)
+
+printf "\n# \e[0;1;32m%s\e[0m\n" "DONE [$(date +%Y-%m-%d\ %H:%M:%S)]"
